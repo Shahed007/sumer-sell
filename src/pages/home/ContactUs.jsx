0d87cfb870b0ba6b213a616627dd39640728e7c8
Shahed <sk0178397@gmial.com>
2023-12-13 16:23:13 +0600

    intrigated email.js with contact us section.

--- src/pages/home/ContactUs.jsx
+++ src/pages/home/ContactUs.jsx
@@ -1,17 +1,50 @@
 import Swal from "sweetalert2";
 import Container from "../../components/Container";
 import Title from "../../components/title/Title";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
+import { useState } from "react";
 
 const ContactUs = () => {
-  const handaleSubmit = (e) => {
-    e.preventDefault();
+  const form = useRef();
+  const [loading, setLoading] = useState(false);
 
-    Swal.fire({
-      title: "Masse send success fully",
-      text: "Please wetting for response",
-      icon: "success",
-    });
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    emailjs
+      .sendForm(
+        "service_o4zckjj",
+        "template_sowrg2o",
+        form.current,
+        "Nnx00uEhziE8ovVZD"
+      )
+      .then(
+        (result) => {
+          if (result.text === "OK") {
+            setLoading(false);
+            Swal.fire({
+              title: "Masse send success fully",
+              text: "Please wetting for response",
+              icon: "success",
+            });
+          }
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error.text);
+        }
+      );
   };
+  // const handaleSubmit = (e) => {
+  //   e.preventDefault();
+
+  //   Swal.fire({
+  //     title: "Masse send success fully",
+  //     text: "Please wetting for response",
+  //     icon: "success",
+  //   });
+  // };
   return (
     <section className="mb-20">
       <Container>
@@ -20,25 +53,26 @@ const ContactUs = () => {
         </div>
 
         <div className="md:max-w-2xl mx-auto">
-          <form onSubmit={handaleSubmit} className="flex flex-col gap-6">
+          <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-6">
             <div className="w-full">
               <div className="relative w-full  h-10">
                 <input
                   required
-                  type="email"
-                  name="email"
+                  type="text"
+                  name="form_name"
                   className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
                   placeholder=" "
                 />
                 <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">
-                  Your Email
+                  Your Name
                 </label>
               </div>
             </div>
             <div className="w-full">
               <div className="relative w-full  h-10">
                 <input
-                  type="text"
+                  type="email"
+                  name="form_email"
                   className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
                   placeholder=" "
                 />
@@ -50,6 +84,7 @@ const ContactUs = () => {
             <div className="w-full">
               <div className="relative w-full ">
                 <textarea
+                  name="message"
                   required
                   className="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
                   placeholder=" "
@@ -60,8 +95,16 @@ const ContactUs = () => {
               </div>
             </div>
 
-            <button className="btn btn-block" type="submit">
-              Send
+            <button
+              disabled={loading}
+              className="btn btn-block flex justify-center items-center"
+              type="submit"
+            >
+              {loading ? (
+                <span className="loading loading-spinner loading-xs"></span>
+              ) : (
+                <span>Send</span>
+              )}
             </button>
           </form>
         </div>
